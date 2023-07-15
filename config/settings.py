079b79991cd1b101d2cade3c4f169a22337c1f88
Alain Ishimwe <alainishimwe2000@gmail.com>
2023-07-15 16:27:33 -0400

    modified settings.py

--- config/settings.py
+++ config/settings.py
@@ -101,9 +101,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-		default: env.db()
-	}
-
+    'default': env.db()
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
