6fcfa0000edf96835c150b04674cc4af389559f4
Alain Ishimwe <alainishimwe2000@gmail.com>
2023-07-17 09:39:38 -0400

    created serializers and views for users and organization

--- safe_connected/views.py
+++ safe_connected/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import User_Lang, Organization, User
+from .serializers import UserLanguageSerializer, OrganizationSerializer, UserSerializer
 
 # Create your views here.
+
+class UserViewSet(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class OrganisationViewSet(generics.ListAPIView):
+    queryset = Organization.objects.all()
+    serializer_class = OrganizationSerializer
