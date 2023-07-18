50b7ad743c1d34d90b7afaa59b8451489223947f
Steve Lehew <120685948+SLehew@users.noreply.github.com>
2023-07-18 10:30:55 -0400

    updated readme with apis for user create and token create

--- config/settings.py
+++ config/settings.py
@@ -21,12 +21,12 @@ env = environ.Env(
     RENDER=(bool, False)
 )
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
