758fc295ea63d7f53b10f82e72ebff2ef53e8acc
Steve Lehew <120685948+SLehew@users.noreply.github.com>
2023-07-18 15:41:27 -0400

    accepted changes

--- safe_connected/models.py
+++ safe_connected/models.py
@@ -101,3 +101,11 @@ class Event(models.Model):
 
     def __str__(self):
         return self.event_title
+
+
+class EventRoster(models.Model):
+    event_id = models.ForeignKey(to=Event, on_delete=models.CASCADE)
+    client_attendee = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.event_id
