8819b8b29e37b42b27d95f7f4d56f46e81a66a8d
Alain Ishimwe <46564647+alainishimwe@users.noreply.github.com>
2023-07-19 12:26:31 -0400

    Update 0010_eventroster_event_manager_and_more.py
    changed default_value for event_organization to 1 to get render running.

--- safe_connected/migrations/0010_eventroster_event_manager_and_more.py
+++ safe_connected/migrations/0010_eventroster_event_manager_and_more.py
@@ -21,7 +21,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='eventroster',
             name='event_organization',
-            field=models.ForeignKey(default=5, on_delete=django.db.models.deletion.CASCADE, to='safe_connected.organizationprofile'),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='safe_connected.organizationprofile'),
             preserve_default=False,
         ),
         migrations.AlterField(
